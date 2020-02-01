8657d27ebe13aa38324f244fb6b85e048bdeda59
Herbert Weigert Venancio <hwvenancio@gmail.com>
2020-02-01 16:43:25 -0300

    added characters

---
+++ Assets/Characters/HectorCrash/HectorCrashDemo.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HectorCrashDemo : MonoBehaviour
+{
+    
+    private Animator anim;
+
+    void Awake()
+    {
+        this.anim = GetComponent<Animator>();
+    }
+    
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            anim.SetTrigger("idle");
+            return;
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            anim.SetTrigger("walk");
+            return;
+        }
+        if(Input.GetKeyDown(KeyCode.D)) {
+            anim.SetTrigger("action");
+            return;
+        }
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            anim.SetTrigger("victory");
+            return;
+        }
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            anim.SetTrigger("defeat");
+            return;
+        }
+    }
+}
