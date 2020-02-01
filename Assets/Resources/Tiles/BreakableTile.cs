9aafc8701f45aa0606715c8c24808757c64dce9f
Marcos Soares <marcos.soares@objective.com.br>
2020-02-01 05:18:34 -0300

    Adicionados novos tipos de tile

---
+++ Assets/Resources/Tiles/BreakableTile.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakableTile : Tile
+{
+    private bool Broken;
+
+    void Start()
+    {
+        IsWalkable = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void Break()
+    {
+        Broken = true;
+        IsWalkable = true;
+        //TODO: Switch state and animation
+    }
+}
